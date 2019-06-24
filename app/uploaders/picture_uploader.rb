33cca8034881e47205a4819db174ba58bbc95c30
Emilio Torres <emiliotorres75@gmail.com>
2019-06-24 18:47:50 +0000

    s3 connection

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,11 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
-  storage :file
+  # storage :file
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
- 
+
 end
