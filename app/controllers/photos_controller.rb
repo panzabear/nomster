7f480bc41eea65754a091654eafd80a227c28468
Emilio Torres <emiliotorres75@gmail.com>
2019-06-24 17:24:14 +0000

    added before_action to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,10 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
-  end
-
-  def destroy
-    @place = Place.find(params[:place_id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-    redirect_to place_path
   end
 
 private
